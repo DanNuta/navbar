80f58056f999c6e9c1e5c18c7624f74f26cb0192
DanNuta <dany.condurari@gmail.com>
2022-01-01 22:15:23 +0200

    add.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './Home';
 
 class App extends Component {
+    
     constructor(props) {
         super(props);
         this.state = {  }
